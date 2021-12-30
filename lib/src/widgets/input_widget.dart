ef29b6d7b2e9215a20c4de54c1d98a4fd9d37d0e
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2021-12-30 09:56:58 -0500

    Update input_widget.dart

--- lib/src/widgets/input_widget.dart
+++ lib/src/widgets/input_widget.dart
@@ -393,7 +393,7 @@ class _InputWidgetView
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           if (!widget.selectorConfig.setSelectorButtonAsPrefixIcon) ...[
             Column(
